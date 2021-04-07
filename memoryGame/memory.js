00cfd962886bee541c099ff22b8f28d1415c0d02
are-you-real <robert.to.smykowski@gmail.com>
2021-04-07 15:13:17 +0200

    restart game added

--- memoryGame/memory.js
+++ memoryGame/memory.js
@@ -1,24 +1,35 @@
 var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png" ];
-cards = cards.sort((a, b) => 0.5 - Math.random());
-
-for (i = 0; i < 12; i++){
-  //$('.score').before('<div class="card" id="c' + i + '"></div>');
-  $('.board').append('<div class="card" id="c' + i + '"></div>');
-}
-var c = new Array(12);
-for (i = 0; i < 12; i++){
-  c[i] = document.getElementById('c'+i);
-}
-for (let i = 0; i < 12; i++){
-  //c[i].addEventListener("click", function() { revealCard(this.id.substring(1)); });
-  c[i].addEventListener("click", function() { revealCard(i); });
-}
 
 var oneVisable = false;
 var turnCounter = 0;
 var visable_nr;
 var lock = false;
 var pairsLeft = 6;
+
+startGame();
+
+
+
+function startGame() {
+  cards = cards.sort((a, b) => 0.5 - Math.random());
+
+  oneVisable = false;
+  turnCounter = 0;
+  lock = false;
+  pairsLeft = 6;
+
+
+  c = new Array(12);
+  for (let i = 0; i < 12; i++){
+    $('.score').before('<div class="card" id="c' + i + '"></div>');
+    c[i] = document.getElementById('c'+i);
+    c[i].addEventListener("click", function() { revealCard(i); });
+  }
+  $('.score').html('Turn conter: ' + turnCounter);
+
+}
+
+
 
 function revealCard(nr) {
 
@@ -55,13 +66,11 @@ function revealCard(nr) {
       turnCounter++;
       $('.score').html('Turn conter: ' + turnCounter);
     }
-
-
   }
-
-
-
 }
+
+
+
 
 function hide2Cards(nr1,nr2) {
   $('#c'+nr1).css('opacity', '0');
@@ -69,9 +78,18 @@ function hide2Cards(nr1,nr2) {
 
   pairsLeft--;
   if(pairsLeft == 0){
-    $('.board').html('<h1>You win!!! <br> Done in ' +turnCounter + 'turns.</h1>');
+    $('#c0').before('<h1>You win!!! <br> Done in ' +turnCounter + 'turns.</h1>');
+    $('#c0').before('<div  id="restart">RESTART</div>');
+    //$('#restart').css('cursor', 'pointer');
+    document.getElementById('restart').addEventListener("click", function() { restartGame(); });
+
   }
 
+  function restartGame() {
+    $('.board div:not(.score)').remove();
+    $('.board h1').remove();
+    startGame();
+  }
 
   lock = false;
 
